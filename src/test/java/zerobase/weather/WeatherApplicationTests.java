f549129f828dbd9f606ed826f2c3b748a3edfcfe
Wang_Seok_Hyeon <wsh096@gmail.com>
2023-03-13 15:11:23 +0900

    init

--- src/test/java/zerobase/weather/WeatherApplicationTests.java
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -1,13 +1,40 @@
 package zerobase.weather;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class WeatherApplicationTests {
 
+   @Test
+   @DisplayName("동일테스트")
+
+   void equalTest(){
+       //given
+       //when
+       //then
+        assertEquals(1,1);
+   }
     @Test
-    void contextLoads() {
+    @DisplayName("nulltest")
+
+    void nullTest(){
+        //given
+        //when
+        //then
+        assertNull(null);
     }
 
+    @Test
+    @DisplayName("trueTest")
+
+    void trueTest(){
+        //given
+        //when
+        //then
+        assertTrue(1==1);
+    }
 }
